9c443a87d64fc81dae1051b5986d06cf1050155d
Nico Coetzee <nicc777@gmail.com>
2021-01-03 09:50:09 +0200

    Started working on repo loading

--- src/pygitsync/pygitsync.py
+++ src/pygitsync/pygitsync.py
@@ -1,8 +1,12 @@
 import argparse
 import os
+import socket
+import traceback
+from git import Repo
 
 
-cwd = os.getcwd()
+CWD = os.getcwd()
+HOSTNAME = socket.gethostname()
 
 
 parser = argparse.ArgumentParser(description='Automatically keep a remote git repo and a local directory in sync')
@@ -10,15 +14,29 @@ parser.add_argument(
     '--path',
     nargs='*',
     help='One or more local Git repository diretories to auto-sync. By default, the local working directory is used.',
-    default=['{}'.format(cwd),],
+    default=['{}'.format(CWD),],
     dest='paths',
     metavar='PATH'
 )
 args = parser.parse_args()
 
 
+def create_repo_obj(path: str)->Repo:
+    try:
+        return Repo(os.path.join(path))
+    except:
+        print('EXCEPTION: {}'.format(traceback.format_exc()))
+    return None
+
+
 def run():
-    print('Running from [{}]'.format(cwd))
+    print('Running from [{}] on host [{}]'.format(CWD, HOSTNAME))
     for path in args.paths:
         print('Current repo directory: {}'.format(path))
+        repo = create_repo_obj(path=path)
+        if repo:
+            print('\tChecking if host branch exists... ', end='')
+            print('ok')
+        else:
+            print('error: path [{}] does not appear to be valid. skipping...'.format(path))
 
